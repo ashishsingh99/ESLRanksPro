ba398286f12bff18f593882eea9e663d8c8705ba
Ashish Kumar <51400448+ashishsingh99@users.noreply.github.com>
2023-04-19 13:06:47 +0530

    Update urls.py

--- djangoapi/urls.py
+++ djangoapi/urls.py
@@ -5,6 +5,5 @@ from account import views
 
 urlpatterns = [
     path('api/user/', include('account.urls')),
-    path('homie',views.HomeView.as_view(), name='Home'),
     re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
